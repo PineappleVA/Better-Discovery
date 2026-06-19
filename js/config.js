26867183d2ad7e8a59e4319fe84c3073c079f413
Jaime Gaming <jaimegamingpro@gmail.com>
2026-06-19 17:49:31 +0200

    Update js/config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 // Si cambias de proyecto, actualiza estas dos constantes.
 // ============================================================
 
-const SUPABASE_URL      = 'https://YOUR_PROJECT_ID.supabase.co';
+const SUPABASE_URL      = 'https://onfgppojanfagdccvrcy.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uZmdwcG9qYW5mYWdkY2N2cmN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4NzA5NjMsImV4cCI6MjA5NzQ0Njk2M30.U93p34ZfBnHND0Xozj-9o2DzrAmvypb053P7274f7cg';
 
 // Detecta automáticamente si las credenciales fueron completadas
