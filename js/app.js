8dc746b8237970133226c5c24164bcfe799ec067
Jaime Gaming <jaimegamingpro@gmail.com>
2026-06-19 17:41:37 +0200

    Apply suggestion from @jaime-gaming

--- js/app.js
+++ js/app.js
@@ -15,7 +15,8 @@ const textEncoder = new TextEncoder();
 if (IS_CONFIGURED) {
   supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
-    SUPABASE_ANON_KEY
+    SUPABASE_ANON_KEY,
+eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9uZmdwcG9qYW5mYWdkY2N2cmN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE4NzA5NjMsImV4cCI6MjA5NzQ0Njk2M30.U93p34ZfBnHND0Xozj-9o2DzrAmvypb053P7274f7cg
   );
 }
 
